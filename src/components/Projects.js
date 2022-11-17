d67e4010b0c3c21df2cd36c86a682e247a85dbdc
benzelcurry <benawhiting@gmail.com>
2022-11-17 13:31:27 -0800

    Add Wheres Waldo project to projects display

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,28 +1,49 @@
 import React from 'react';
 
 import Letterboxd from '../images/letterboxd-clone.png';
+import Waldo from '../images/waldo.png';
 import '../stylesheets/Projects.css';
 
 const Projects = () => {
   return (
     <div className="projects-container">
       <h2 className="projects-title">Projects</h2>
-      <div className='project'>
-        <img src={Letterboxd} alt='Letterboxd clone' className='screenshot' />
-        <div className="project-info">
-          <div className="project-name">Letterboxd Clone</div>
-          <div className="project-description">
-            Clone of letterboxd.com, built from the ground up using React, The Movie
-            Database API, and Firebase Hosting.
+      <div className="projects-display">
+        <div className='project'>
+          <img src={Letterboxd} alt='Letterboxd clone' className='screenshot' />
+          <div className="project-info">
+            <div className="project-name">Letterboxd Clone</div>
+            <div className="project-description">
+              Clone of letterboxd.com, built from the ground up using React, The Movie
+              Database API, and Firebase Hosting.
+            </div>
+            <div className="buttons">
+              <a href="https://letterboxd-clone-odin.web.app/" target='_blank' rel='noreferrer'>
+                <button>Live App</button>
+              </a>
+              <a href="https://github.com/benzelcurry/letterboxd-clone" target='_blank' rel='noreferrer'>
+                <button>Repository</button>
+              </a>
+            </div>
           </div>
-          <div className="buttons">
-            <a href="https://letterboxd-clone-odin.web.app/" target='_blank' rel='noreferrer'>
-              <button>Live App</button>
-            </a>
-            <a href="https://github.com/benzelcurry/letterboxd-clone" target='_blank' rel='noreferrer'>
-              <button>Repository</button>
-            </a>
+        </div>
+        <div className="project">
+          <div className="project-info">
+            <div className="project-name">Where's Waldo</div>
+            <div className="project-description">
+              Where's Waldo web app, built using React, Firestore Database, and Firebase
+              hosting.
+            </div>
+            <div className="buttons">
+              <a href="https://wheres-waldo-at.web.app/" target='_blank' rel='noreferrer'>
+                <button>Live App</button>
+              </a>
+              <a href="https://github.com/benzelcurry/wheres-waldo" target='_blank' rel='noreferrer'>
+                <button>Repository</button>
+              </a>
+            </div>
           </div>
+          <img src={Waldo} alt="Where's Waldo" className='screenshot' />
         </div>
       </div>
     </div>
