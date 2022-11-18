2a4c985f5af8e01cbc8f9939329708acf2c03a8a
Ben <benawhiting@gmail.com>
2022-11-18 15:15:43 -0800

    Add icons that link to social platforms in contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,23 @@ const Contact = () => {
   return (
     <div className="contact-container">
       <div className="contact-title">Contact</div>
+      <div className="contact-status">
+        I am currently seeking a position as either a front end or full stack 
+        web developer. If you're interested in learning more about either me or my
+        projects, feel free to reach out to me below!
+      </div>
+      <div className="contact-form">
+        <a href="https://github.com/benzelcurry" target='_blank' rel='noreferrer'>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            alt='Github' className='contact-icon gh'
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/ben-whiting-563526207/" target='_blank' rel='noreferrer'>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+            alt='LinkedIn' className='contact-icon'
+          />
+        </a>
+      </div>
     </div>
   );
 };
